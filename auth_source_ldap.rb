08c13d8c99a100d8eed0dadfda1250e6906e7c0b
Willam <81498316+willam2007@users.noreply.github.com>
2024-12-04 17:05:44 +1000

    Update auth_source_ldap.rb

--- auth_source_ldap.rb
+++ auth_source_ldap.rb
@@ -1,3 +1,85 @@
+ def sync_departments
+    begin
+      # Инициализируем соединение с LDAP
+      ldap_con = initialize_ldap_con(self.account, self.account_password)
+      division_filter = Net::LDAP::Filter.eq("objectClass", "organizationalUnit")
+      # Поиск подразделений в LDAP
+      ldap_entries = ldap_con.search(
+        base: self.base_dn,
+        filter: division_filter,
+        # все уровни вложности
+        scope: Net::LDAP::SearchScope_WholeSubtree,
+        attributes: ['physicalDeliveryOfficeName','ou','name', 'distinguishedname']
+      )
+      if ldap_entries.nil? || ldap_entries.empty?
+        return
+      else
+        Rails.logger.info "Search: #{ldap_entries.size}"
+      end
+
+      # Создаем хеш подразделений из LDAP
+      ldap_departments = {}
+      ldap_entries.each do |entry|
+        department_code = AuthSourceLdap.get_attr(entry, 'physicalDeliveryOfficeName')
+        name = AuthSourceLdap.get_attr(entry, 'ou')
+
+        next unless department_code.present? && name.present?
+
+        if ldap_departments.key?(department_code) || ldap_departments.key?(name)
+          Rails.logger.warn "Double ldap_departments"
+          next
+        end
+         clean_name = name.strip
+         if clean_name[0] =~ /^\d/
+           Rails.logger.warn "skip #{name}, beceause if numbers at the beginnning"
+           next
+         end
+
+
+        ldap_departments[department_code] = {name: name, department_code: department_code, dn: entry.dn }
+      end
+      existing_departments = Department.all.index_by(&:department_code)
+      ldap_department_codes = ldap_departments.keys
+
+      # Синхронизация подразделений
+      ldap_departments.each do |department_code, data|
+        name = data[:name]
+        department_code = data[:department_code]
+
+        if existing_departments.key?(department_code)
+          department = existing_departments[department_code]
+          department.name = name
+          department.background = "Sync from AD"
+        else
+          department = Department.new(
+            name: name,
+            department_code: department_code,
+            background: "Add from AD"
+          )
+        end
+
+        if department.save
+          Rails.logger.info "Succefull save #{department.name}"
+        else
+          Rails.logger.error "Error save #{department.name}"
+        end
+      end
+
+      # Удаляем подразделения, отсутствующие в LDAP
+      departments_to_delete = existing_departments.keys - ldap_department_codes
+      departments_to_delete.each do |department_code|
+        department = existing_departments[department_code]
+        department.destroy
+        Rails.logger.info "Delete departments #{department.name}"
+      end
+      Rails.logger.info "END SYNC DEPARTMENTS"
+    rescue => e
+      Rails.logger.error "Warn in def sync_departments: #{e.message}"
+      Rails.logger.error e.backtrace.join("\n")
+      raise
+    end
+  end
+
 DN: OU=Отдел развития банковских технологий,OU=Основной офис,OU=Владивосток,OU=PSKB,DC=pskb,DC=ad
 dn: ["OU=Отдел развития банковских технологий,OU=Основной офис,OU=Владивосток,OU=PSKB,DC=pskb,DC=ad"]
 objectclass: ["top", "organizationalUnit"]
